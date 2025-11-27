409114882f5e7126df5ca4606097bc24e3defb3b
Hideki Kobayashi <hkobayashi@icp.uni-stuttgart.de>
2025-11-27 12:55:07 +0100

    Resolve heatup using vv within ASE

---
+++ samples/lj_liquid_ase.py
@@ -0,0 +1,144 @@
+#
+# Copyright (C) 2013-2022 The ESPResSo project
+#
+# This file is part of ESPResSo.
+#
+# ESPResSo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ESPResSo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Demonstrate the ESPResSo to ASE interface.
+Simulate a Lennard-Jones fluid maintained at a fixed temperature
+by a Langevin thermostat.
+"""
+import numpy as np
+import espressomd
+import espressomd.plugins.ase
+from ase.calculators.lj import LennardJones
+
+required_features = ["LENNARD_JONES"]
+espressomd.assert_features(required_features)
+
+# System parameters
+
+box_l = 10.7437
+density = 0.7
+
+# Interaction parameters (repulsive Lennard-Jones)
+
+lj_eps = 1.0
+lj_sig = 1.0
+lj_cut = 2.**(1 / 6) * lj_sig
+
+# Integration parameters
+system = espressomd.System(box_l=[box_l] * 3)
+np.random.seed(seed=42)
+
+system.time_step = 0.005
+system.cell_system.skin = 0.4
+
+warm_steps = 10
+int_steps = 100
+int_n_times = 5000 * 5
+
+
+# Particle setup
+
+volume = system.volume()
+n_part = int(volume * density)
+
+system.part.add(pos=np.random.random((n_part, 3)) * system.box_l)
+
+
+## Setup of ase interface and Lennard-Jones calculator
+
+# Mapping of ESPResSo types to ASE types
+
+type_mapping = {0: 0}
+
+system.ase = espressomd.plugins.ase.ASEInterface(
+    #system,
+    type_mapping)
+    #system.part.all())
+
+# ASE calculator tor provide Lennard-Jones forces
+lj = LennardJones(sigma=lj_sig, epsilon=lj_eps, rc=lj_cut, smooth=False)
+#ase.atoms.calc = lj
+# Overlap removal via steepest descent
+
+system.integrator.set_steepest_descent(f_max=0, gamma=1e-3,
+                                       max_displacement=lj_sig / 100)
+
+
+# Integrate one step to get valid particle forces
+#ase.integrate(1, lj)
+atoms = system.ase.get()
+atoms.calc = lj
+system.part.all().ext_force = atoms.get_forces()
+system.integrator.run(1)
+
+max_force = np.abs(np.amax(system.part.all().f))
+while max_force > 10:
+    #ase.integrate(warm_steps, lj)
+    for _ in range(warm_steps):
+        atoms = system.ase.get()
+        atoms.calc = lj
+        system.part.all().ext_force = atoms.get_forces()
+        system.integrator.run(1)
+    max_force = np.abs(np.amax(system.part.all().f))
+    print("Overlap removal:", max_force)
+
+
+# Normal integration
+
+
+system.integrator.set_vv()
+#system.integrator.set_symplectic_euler()
+
+
+# system.non_bonded_inter[0,0].lennard_jones.set_params(epsilon=lj_eps,sigma=lj_sig, cutoff=lj_cut, shift='auto')
+system.integrator.run(0, reuse_forces=False)
+forces = system.part.all().f
+#ase.update_ase()
+atoms = system.ase.get()
+ase_pos = [a.position for a in atoms]
+np.testing.assert_allclose(ase_pos, system.part.all().pos)
+
+lj = LennardJones(sigma=lj_sig, epsilon=lj_eps, rc=lj_cut, smooth=False)
+#ase.atoms.calc = lj
+#ase.update_ase()
+#ase_forces = ase.atoms.get_forces()
+
+# activate thermostat
+system.thermostat.set_langevin(kT=1, gamma=1.0, seed=42)
+p = system.part.by_id(0)
+print(p.f, p.ext_force, p.v)
+for i in range(10):
+    #ase.integrate(1, lj)
+    atoms = system.ase.get(integrator="vv")
+    #atoms = system.ase.get()
+    atoms.calc = lj
+    system.part.all().ext_force = atoms.get_forces()
+    system.integrator.run(1, reuse_forces=True)
+
+for i in range(int_n_times):
+    print(f"run {i} at time={system.time:.2f}")
+
+    #ase.integrate(int_steps, lj)
+    atoms = system.ase.get(integrator="vv")
+    #atoms, pos = system.ase.get()
+    atoms.calc = lj
+    system.part.all().ext_force = atoms.get_forces()
+    system.integrator.run(1, reuse_forces=True)
+    print(1 / n_part * np.sum(system.part.all().v**2) / 2)
+    #print(1 / n_part * np.sum(system.part.all().v**2) / 2, p.pos - pos[0] - system.time_step * p.v)
--- src/python/espressomd/plugins/ase.py
+++ src/python/espressomd/plugins/ase.py
@@ -38,17 +38,76 @@ class ASEInterface:
         """Register the system."""
         self._system = system
 
-    def get(self, folded=False) -> ase.Atoms:
+    def get(self, folded=False, integrator="none") -> ase.Atoms:
         """Export the ESPResSo system particle data to an ASE atoms object."""
         particles = self._system.part.all()
         positions = np.copy(particles.pos_folded if folded else particles.pos)
+        velocities = np.copy(particles.v)
         types = np.copy(particles.type)
         forces = np.copy(particles.f)
         if any(p.is_virtual() for p in particles):
             raise RuntimeError("ASE doesn't support virtual sites")
 
+        # We assume here, for simplicity, that
+        # the interaction is computed via ASE only.
+        # In the present implementation,
+        # the ASE-based force evaluation proceeds as follows:
+        #   atoms = system.ase.get()
+        #   atoms.calc = lj
+        #   system.part.all().ext_force = atoms.get_forces()
+        #   system.integrator.run(1)
+        #
+        # Within system.integrator.run(1), the following sequence is executed:
+        #   1. calculate_forces()
+        #      p.force() = p.ext_force()
+        #      p.force() += langevin_noise()
+        #   2. integrator_step1()
+        #   3. calculate_forces()
+        #   4. integrator_step2()
+        # If external_force() is evaluated using the posistion at time r(t),
+        # then the particle forces are updated according to r(t)
+        # before the velocities are advanced.
+        # Consequently, in implemented symlectic Euler scheme,
+        # r(t), v(t), F(t) are evolve as:
+        #   F = F(t)
+        #   v = v(t) + dt * F / m
+        #   r = r(t) + dt * v
+        # This correspond exactly the standard symlectic Euler integrator.
+        #
+        # In contrast, the curreently implemented velocity verlet scheme updates
+        # r(t), v(t) and F(t) as follows:
+        #   F = F(t)
+        #   v = v(t) + 0.5 * dt * F / m
+        #   r = r(t) + dt * v
+        #   F = F(t) (because p.ext_force() is not recomputed)
+        #   v = v + 0.5 * dt *F / m
+        # This doses NOT correspond to a true velocity verlet update.
+        # Thereore this leads to an artificial increase in system temperature.
+        #
+        # To recover a correct velocity verlet implementation,
+        # we manually compute r(t+dt) from r(t), v(t) and F(t),
+        # and then evaluate p.ext_force() using r(t+dt).
+        # if ASE integration is constructed as:
+        #   atoms = system.ase.get(integrator="vv")
+        #   atoms.calc = lj
+        #   system.part.all().ext_force = atoms.get_forces()
+        #   system.integrator.run(1, reuse_forces=True)
+        # then the updates of r(t), v(t), F(t) become:
+        #   F = F(t) (identical to the last step due to reuse_forces=true)
+        #   v = v(t) + 0.5 * dt * F / m
+        #   r = r(t) + dt * v
+        #   F = F(t + dt) (because p.ext_force() is evaluated at the updated positions)
+        #   v = v + 0.5 * dt *F / m
+        # This sequence ideintical with the standard velocity verlet scheme.
+        if integrator == "vv":
+            v = velocities + 0.5 * forces * self._system.time_step
+            pos = positions + v * self._system.time_step
+        else:
+            pos = positions
+
         atoms = ase.Atoms(
-            positions=positions,
+            #positions=positions,
+            positions=pos,
             numbers=types,
             pbc=np.copy(self._system.periodicity),
             cell=np.copy(self._system.box_l),
